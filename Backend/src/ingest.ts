38ce59f5d1bb57f65ab4372c19f1257abd660808
GGGP-Test <ali.galacticgg@gmail.com>
2025-08-20 21:40:32 +0330

    Create ingest.ts

---
+++ Backend/src/ingest.ts
@@ -0,0 +1,4 @@
+// @ts-nocheck
+export { pollSocialFeeds as runIngestOnce } from './connectors/socialFirehose.js';
+export { pollSocialFeeds } from './connectors/socialFirehose.js';
+export default {};
