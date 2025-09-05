8940bc0b08a9ac676e586933323788716c26d3eb
GGGP-Test <ali.galacticgg@gmail.com>
2025-09-05 17:26:23 +0330

    Create vitest.config.ts

---
+++ Backend/vitest.config.ts
@@ -0,0 +1,12 @@
+// vitest.config.ts
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    environment: "node",
+    include: ["tests/**/*.spec.ts"],
+    coverage: {
+      reporter: ["text", "html"],
+    },
+  },
+});
