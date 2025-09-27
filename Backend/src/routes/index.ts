18648ad5d902fcc91d1ff7935e9ce25e63558030
GGGP-Test <ali.galacticgg@gmail.com>
2025-09-28 03:26:55 +0330

    Delete Backend/src/routes/index.ts

--- Backend/src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-// src/routes/index.ts
-import { Router } from "express";
-import leadsRouter from "./leads";
-
-// If you add more route groups later (metrics, auth, etc), mount them here.
-const router = Router();
-router.use("/leads", leadsRouter);
-
-export default router;
