fc01a2562752897a1420cac51be4b681b56f6f56
GGGP-Test <ali.galacticgg@gmail.com>
2025-11-15 03:34:13 +0330

    Update process.step5.js

--- docs/sections/process/steps/process.step5.js
+++ docs/sections/process/steps/process.step5.js
@@ -148,11 +148,11 @@
       // Mobile headings
       M_HEAD_BOX_H: null,
       M_HEAD_SPACING: null,
-      M_HEAD_OFFSET_Y: 220,
+      M_HEAD_OFFSET_Y: 200,
       M_HEAD_MAX_WIDTH_PCT: null,
       M_HEAD_BASELINE_BIAS: null,
       M_TITLE_OFFSET_X: null,
-      M_TITLE_OFFSET_Y: 250,
+      M_TITLE_OFFSET_Y: -250,
 
       // mobile-specific padding from last item to first dot
       M_DOTS_TOP_PAD: 30,
